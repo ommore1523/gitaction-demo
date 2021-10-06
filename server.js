886f7aa3aea7aaf33bb42b940799ff1cffff9ba1
omkar more <52170865+ommore1523@users.noreply.github.com>
2021-10-06 11:17:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World omkar');
+  res.send('Hello World omkar more');
 });
 
 app.listen(PORT, HOST);
